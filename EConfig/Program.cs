6e5c355f203d6b11290f65be47151c81e80c6ec8
Joost Schriek <joost.schriek@gemalto.com>
2019-07-29 16:34:08 -0400

    wip

--- EConfig/Program.cs
+++ EConfig/Program.cs
@@ -6,6 +6,7 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
+using EConfig.Services;
 using Jil;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
@@ -16,6 +17,8 @@ using Org.BouncyCastle.Asn1.Pkcs;
 using Org.BouncyCastle.Asn1.X509;
 using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.X509;
+using static EConfig.Helpers;
+
 
 namespace EConfig
 {
@@ -23,100 +26,17 @@ namespace EConfig
     {
         private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
 
-        private static string configFilename = "appsettings.json";
-
         public static void Main(string[] args)
         {
-            bool load = false;
-
-            var keyOptions = new OptionSet
-            {
-                {"load", "Verifies and makes the key findable for the later operations.", s => load = true}
-
-            };
-            var encryptOptions = new OptionSet
-            {
-                {"file", "The file to load and encrypt (defaults to appsettings.json)", s => configFilename = s }
-            };
+            
 
             var cmds = new CommandSet("EncryptedConfig")
             {
-                new Command("key", "Key generation and loading. Will wirte a new key to the output ")
-                {  
-                    Options = keyOptions,
-                    Run = pargs => { GenerateAndWriteKeys() }
-                },
-                new Command("encrypt")
-                {
-                    Options = encryptOptions,
-                    Run = Encrypt
-                }
+                new KeyCommand(),
+                new EncryptionCommand("encrypt")
             };
             
             cmds.Run(args);
-        }
-        
-
-        private static void Encrypt(IEnumerable<string> pargs)
-        {
-            // We assume there is a property in at the root of the json that is called publickKey and holds our public key for writing values.
-            var publickKey = FindPublicKey(configFilename);
-        }
-
-
-        private static AsymmetricKeyParameter FindPublicKey(string filename)
-        {
-            AsymmetricKeyParameter publikcKey = null;
-
-            Dictionary<string, object> config;
-            using (var configStream = File.OpenText(filename))
-            {
-                config = JSON.Deserialize<Dictionary<string, object>>(configStream);
-            }
-
-            string hexedPublicKey;
-            if (config.ContainsKey("publicKey"))
-            {
-                var bytesPublickey = FromHexToByte(config["publicKey"] as string);
-                publikcKey = PublicKeyFactory.CreateKey(bytesPublickey);
-            }
-
-            return publikcKey;
-        }
-
-
-        private static void GenerateAndWriteKeys()
-        {
-            var keypair = GenerateKeypair();
-
-            PrivateKeyInfo privateKeyInfo = PrivateKeyInfoFactory.CreatePrivateKeyInfo(keypair.Private);
-            SubjectPublicKeyInfo publicKeyInfo = SubjectPublicKeyInfoFactory.CreateSubjectPublicKeyInfo(keypair.Public);
-            
-            logger.Info($"public: {FromByteToHex(publicKeyInfo.ToAsn1Object().GetDerEncoded())}");
-            logger.Info($"private: {FromByteToHex(privateKeyInfo.ToAsn1Object().GetDerEncoded())}");
-        }
-
-        private static byte[] FromHexToByte(string hextacular) => Enumerable
-                .Range(0, hextacular.Length)
-                .Where(i => i % 2 == 0)
-                .Select(i => Convert.ToByte(hextacular.Substring(i, 2), 16))
-                .ToArray();
-
-        private static string FromByteToHex(byte[] biting) => BitConverter.ToString(biting).Replace("-", string.Empty);
-        
-        private static AsymmetricCipherKeyPair GenerateKeypair()
-        {
-            byte[] seed = new byte[16];
-            RNGCryptoServiceProvider a = new RNGCryptoServiceProvider();
-            a.GetNonZeroBytes(seed);
-
-            var secureRandom = new SecureRandom();
-            secureRandom.SetSeed(seed);
-
-            RsaKeyPairGenerator rsaGenny = new RsaKeyPairGenerator();
-            rsaGenny.Init(new KeyGenerationParameters(secureRandom, 32));
-
-            return rsaGenny.GenerateKeyPair();
         }
     }
 }
